b403be7a3fd00a92d201f92327d39d445bb57759
Ben-Kaniobi <Ben_Kaniobi@gmx.net>
2014-04-29 15:27:20 +0200

    Implemented most of class tetromino

--- src/app/Tetromino.cpp
+++ src/app/Tetromino.cpp
@@ -7,3 +7,45 @@
 
 #include "Tetromino.hpp"
 
+Tetromino::Tetromino(Point2D Position) {
+	/* Set start position */
+	this->position.x = Position.x;
+	this->position.y = Position.y;
+	/* Set start rotition */
+	this->rotation = rotZero;
+};
+
+void Tetromino::move(Point2D Dir) {
+	/* TODO: Check if tetromino at new position isfree */
+
+	this->position.x += Dir.x;
+	this->position.y += Dir.y;
+}
+
+void Tetromino::rotate(bool Cw) {
+	/* TODO: Check if tetromino at new position isfree */
+
+	/* Set new enum value in a switch to avoid casting invalid values */
+	Rotation new_rot;
+	if(Cw) {
+		/* Rotate clock wise */
+		switch(this->rotation) {
+		case rotZero:       new_rot = rotNinety;     break;
+		case rotNinety:     new_rot = rotOneeighty;  break;
+		case rotOneeighty:  new_rot = rotTwoseventy; break;
+		case rotTwoseventy: new_rot = rotZero;       break;
+		default:            /* Invalid value */      break;
+		}
+	}
+	else {
+		/* Rotate coutner clock wise */
+		switch(this->rotation) {
+		case rotTwoseventy: new_rot = rotOneeighty;  break;
+		case rotOneeighty:  new_rot = rotNinety;     break;
+		case rotNinety:     new_rot = rotZero;       break;
+		case rotZero:       new_rot = rotTwoseventy; break;
+		default:            /* Invalid value */      break;
+		}
+	}
+	this->rotation = new_rot;
+}
--- src/app/Tetromino.hpp
+++ src/app/Tetromino.hpp
@@ -8,7 +8,22 @@
 #ifndef TETROMINO_HPP_
 #define TETROMINO_HPP_
 
+#include "Libraries.hpp"
+
 class Tetromino {
+protected:
+	/* Attributes */
+	Point2D position;
+	Rotation rotation;
+
+	/* Constructor protected for abstract class */
+	Tetromino(Point2D Position);
+
+public:
+	/* Destructor has to be virtual */
+	virtual ~Tetromino() {};
+	void move(Point2D Dir);
+	void rotate(bool Cw);
 };
 
 #endif /* TETROMINO_HPP_ */
