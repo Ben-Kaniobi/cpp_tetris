ee02b9edea6862acb101816043b4c915406dd432
Ben-Kaniobi <Ben_Kaniobi@gmx.net>
2014-04-30 09:58:21 +0200

    Added random tetromino generating

--- src/app/Map.cpp
+++ src/app/Map.cpp
@@ -5,15 +5,30 @@
  *      Author: nicola
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "Map.hpp"
+#include "TetrominoI.hpp"
+#include "TetrominoO.hpp"
+#include "TetrominoT.hpp"
+#include "TetrominoJ.hpp"
+#include "TetrominoL.hpp"
+#include "TetrominoS.hpp"
+#include "TetrominoZ.hpp"
 
 /* Global static pointer used to ensure a single instance of the class */
 Map* Map::TheMap = 0;
 
+/* Variable for random number */
+unsigned int random;
+
 Map* Map::getInstance() {
-	/* Generate the single object on first call */
 	if (!TheMap) {
+		/* Generate the single object on first call */
 		TheMap = new Map;
+		/* Init random seed with current time */
+		srand(time(NULL));
 	}
 	/* Return the single object */
 	return TheMap;
@@ -30,4 +45,18 @@ void Map::removeLine(int yPos) {
 
 void Map::mergeToMap(Tetromino &theTetromino) {
 	//TODO
+
+	/* Generate a random number in range 0 to 6 */
+	random = rand() % 7;
+	/* Replace current tetromino with a new random one */
+	switch(random) {
+	case 0: theTetromino = TetrominoI(); break;
+	case 1: theTetromino = TetrominoO(); break;
+	case 2: theTetromino = TetrominoT(); break;
+	case 3: theTetromino = TetrominoJ(); break;
+	case 4: theTetromino = TetrominoL(); break;
+	case 5: theTetromino = TetrominoS(); break;
+	case 6: theTetromino = TetrominoZ(); break;
+	default: /* Not possible */ break;
+	}
 }
