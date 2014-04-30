195cb572b74b11494ee6eee7fdb3194a3a19ef24
Ben-Kaniobi <Ben_Kaniobi@gmx.net>
2014-04-30 15:56:23 +0200

    Fixed block position drawing

--- src/app/Tetromino4.cpp
+++ src/app/Tetromino4.cpp
@@ -18,7 +18,7 @@ std::string itos(int i){
 
 /* Definition of static const, Tick time in ms */
 const Point2D Tetromino4::startPos = {
-	static_cast<int>(MAP_WIDTH/2),
+	static_cast<int>(MAP_WIDTH/2)-1,
 	-2  /* So it's not visible just yet */};
 
 Tetromino4::Tetromino4(ColorType Color)
@@ -40,12 +40,14 @@ void Tetromino4::rotate(bool Cw) {
 
 void Tetromino4::draw() {
 	/* Draw each square */
-	for(unsigned int i=0; i<4; i++) {
-		if(squares[i].getType() == typeBlock) {
-			int x = position.x*SQUARE_WIDTH;
-			int y = position.y*SQUARE_WIDTH;
-			if(y >= 0) {
-				DrawFilledRectangle(x, y, SQUARE_WIDTH, SQUARE_WIDTH, squares[i].getColor(), 0);
+	for(unsigned int i=0; i<2; i++) {
+		for(unsigned int j=0; j<2; j++) {
+			if(squares[i+j].getType() == typeBlock) {
+				int x = (position.x + i%2) * SQUARE_WIDTH;
+				int y = (position.y + j%2) * SQUARE_WIDTH;
+				if(y >= 0) {
+					DrawFilledRectangle(x, y, SQUARE_WIDTH, SQUARE_WIDTH, squares[i+j].getColor(), 0);
+				}
 			}
 		}
 	}
